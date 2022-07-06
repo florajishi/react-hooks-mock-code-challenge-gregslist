9690a97aff265a86502d67b0d08389cff8f991b8
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2022-07-06 12:53:13 -0500

    set up listing container and cards from fetch data

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 // import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+  const [listings, setListings] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then(r => r.json())
+      .then(listings => setListings(listings))
+  }, [])
+
+  const listingCards = listings.map(listingObj => {
+    return <ListingCard key={listingObj.id} listing={listingObj} />
+  })
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingCards}
       </ul>
     </main>
   );
